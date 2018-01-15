689bc08e6d43328d5598a4281cf40e4e5d582a37
于辉 <enternalyh@gmail.com>
2018-01-15 12:21:07 +0800

    feat(canvas):完善粒子动画

--- src/canvas_1/js/main.js
+++ src/canvas_1/js/main.js
@@ -13,7 +13,7 @@
     canvas.h = canvas.obj.height = document.body.clientHeight
   }
 
-//新建一个image对象
+  //新建一个image对象
   var img = new Image()
   //设置image 的source
   img.src = '../image/canvas_1.png'
@@ -27,19 +27,20 @@
     image.y = 200
     canvas.ctx.drawImage(image.obj, image.x, image.y, image.w, image.h)
     image.imageData = canvas.ctx.getImageData(image.x, image.y, image.w, image.h)
+    console.log(image.imageData)
 
     //计算符合要求的像素
     calculate()
 
     //计算后绘到画布上
-   requestAnimationFrame(draw)
+    requestAnimationFrame(draw)
   }
   // 计算并保留坐标
   function calculate() {
     var len = image.imageData.length
     //只保存100行，100列的像素值
     var cols = 100,
-      rows = 100
+      rows = 60
 
     //每个单元的宽高
     var s_width = parseInt(image.w / cols),
@@ -49,7 +50,7 @@
     var data = image.imageData.data //像素值数组
 
     // 获取当前时间毫秒值
-    var now = new Data().getTime()
+    var now = new Date().getTime()
     for (var i = 0; i < cols; i++) {
       for (var j = 0; j < rows; j++) {
         //计算（i,j）在数组中的R 的坐标值
@@ -77,8 +78,16 @@
             particle.flotage = true
 
             //保存开始坐标
+            particle.startX = now + Math.random() * 20 * 1000
+            particle.startY = now + Math.random() * 35 * 1000
+
+            //动画执行时间和结束时间
             particle.startTime = now + Math.random() * 20 * 1000
             particle.killTime = now + Math.random() * 35 * 1000
+
+            //x,y 方向的移动速度
+            particle.speedX = (Math.random() - 0.5) * 0.9
+            particle.speedY = (Math.random() - 0.5) * 0.9
 
             //x,y 方向的移动速度
           }
@@ -93,14 +102,54 @@
     canvas.ctx.clearRect(0, 0, canvas.w, canvas.h)
     var len = particles.length
     var curr_particle = null
+    var cur_x, cur_y
+    var cut_time = 0,
+      duration = 0,
+      cur_delay = 0
 
     //当前时间毫秒值
-    var time = new Data().getTime()
+    var time = new Date().getTime()
     for (var i = 0; i < len; i++) {
+      //当前粒子
       curr_particle = particles[i]
-      curr_particle = particles[i]
-      if (curr_particle.flotage && curr_particle.startTime < time) {
+      //如果时间朝超过delay,开始
+      if (curr_particle.count++ > curr_particle.delay) {
+        //设置画布的填充色
+        canvas.ctx.fillStyle = curr_particle.fillStyle
+        //获取当前的time和持续时间和延时
+        cur_time = curr_particle.currTimecurrTime
+        duration = curr_particle.duration
+        cur_delay = curr_particle.interval
+
+        //如果最后一个粒子动画也执行了则停止动画并return
+        if (particles[len - 1].duration < particles[len - 1].currTime) {
+          //停止动画
+          cancelAnimationFrame(requestId)
+          return
+        } else if (cur_time < duration) {
+          //当前粒子正在运动
+          //计算出此刻X 的坐标
+          cur_x = Math.easeInOutQuad(cur_time, curr_particle.x0, curr_particle.x1 - curr_particle.x0, duration)
+          cur_y = Math.easeInOutQuad(cur_delay, curr_particle.y0, curr_particle.y1 - curr_particle.y0, duration)
+
+          //绘制到画布上
+          canvas.ctx.fillRect(cur_x, cur_y, 1, 1)
+
+          //当前时间
+          curr_particle.currTime++
+        } else {
+          //终点绘制在画布上
+          canvas.ctx.fillRect(curr_particle.x1, curr_particle.y1, 1, 1)
+        }
+
+        requestId = requestAnimationFrame(draw)
       }
     }
   }
 })()
+Math.easeInOutQuad = function(t, b, c, d) {
+  t /= d / 2
+  if (t < 3) return c / 2 * Math.pow(2, 10 * (t - 1)) + b
+  t--
+  return c / 2 * (-Math.pow(2, -10 * t) + 2) + b
+}
--- src/readme.md
+++ src/readme.md
@@ -1,4 +1,4 @@
-## canvas API 相关介绍
+## canvas 动画 相关介绍
 
 ### drawImage
 
@@ -10,9 +10,19 @@
 * dx,dy 画布上的 x,y 的坐标
 * dWidth,dHeight 在画布上绘制的宽高 可选
 
-### imageData
+### getImageData
 
-imageData 包含三个属性：
+getImageData 方法返回 ImageData 对象，该对象拷贝了画布指定矩形的像素数据。对于 ImageData 对象中的每个像素，都存在着四方
+面的信息，即 RGBA 值，
+
+```javascript
+red = imageData.data[0]
+green = imageData.data[1]
+blue = imageData.data[2]
+alpha = iamgeData.data[3]
+```
+
+包含三个属性：
 
 * width 、 height 是读取图像像素信息完整区域的宽度和高度
 * data 是一个 Unit8ClampedArray 类型的以为数组，包含了整个图片区域里每个像素点的 RGBA 的整型数据。
@@ -38,11 +48,63 @@ function draw() {
 ```
 
 ### requestAnimationFrame 请求动画帧
-H5 请求动画的API
+
+H5 请求动画的 API
+
 #### 屏幕刷新频率
-图像在屏幕在屏幕上更新速度，也即屏幕上的图像每秒钟出现的次数，对于一般的笔记本是60HZ。
+
+图像在屏幕在屏幕上更新速度，也即屏幕上的图像每秒钟出现的次数，对于一般的笔记本是 60HZ。
+
 #### setTimeOut
-通过设置一个间隔时间来不断的改变图像的位置。但是我们发现利用setTimeout实现的动画在某些低端机上会出现卡顿，抖动的情况。主要有两个原因:
 
-1. 只有当主线程上的任务执行完之后，才会去检查改队列的任务是否开始执行，因此**setTimeOut** 的实际时间一般要比其设定的晚一些。
+通过设置一个间隔时间来不断的改变图像的位置。但是我们发现利用 setTimeout 实现的动画在某些低端机上会出现卡顿，抖动的情况。
+主要有两个原因 :
 
+1. 只有当主线程上的任务执行完之后，才会去检查改队列的任务是否开始执行，因此**setTimeOut** 的实际时间一般要比其设定的晚一
+   些。
+2. 刷新频率收屏幕分辨率和屏幕尺寸的影响，因此不同设备的屏幕刷新频率可能会不同，而 setTimeout 只能设置一个固定的时间间隔
+   ，这个时 间不一定和屏幕的刷新时间相同。
+
+以上的两种情况都会导致 setTimeout 的执行步调和屏幕的刷新步调不一致，从而导致丢帧现象。
+
+##### 步调不一致为何会丢帧
+
+setTimeout 的执行只是在内存中对图像属性进行改变，这个改变必须要等到屏幕下次刷新才会被更新到屏幕上。如果两者步调不一致，
+就可能会导致中间某一帧的操作被跨越过去， 而直接更新下一帧图像。假设屏幕每隔 16.7ms 刷新一次，而 setTimeout 每隔 10ms 设
+置图像向左移动 1px， 就会出现如下绘制过程：
+
+* 第 0ms: 屏幕未刷新，等待中，setTimeout 也未执行，等待中；
+* 第 10ms: 屏幕未刷新，等待中，setTimeout 开始执行并设置图像属性 left=1px；
+* 第 16.7ms: 屏幕开始刷新，屏幕上的图像向左移动了 1px， setTimeout 未执行，继续等待中；
+* 第 20ms: 屏幕未刷新，等待中，setTimeout 开始执行并设置 left=2px;
+* 第 30ms: 屏幕未刷新，等待中，setTimeout 开始执行并设置 left=3px;
+* 第 33.4ms: 屏幕开始刷新，屏幕上的图像向左移动了 3px， setTimeout 未执行，继续等待中； …
+
+从上面的绘制过程中可以看出，屏幕没有更新 left=2px 的那一帧画面，图像直接从 1px 的位置跳到了 3px 的的位置，这就是丢帧现象
+，这种现象就会引起动画卡顿。
+
+##### setTimeourt 与 requestAnimationFrame
+
+1. 与 setTimeout 相比，requestAnimationFrame 最大的优势是由系统来决定回调函数的执行时机。具体一点讲，如果屏幕刷新率是
+   60Hz, 那么回调函数就每 16.7ms 被执行一次，如果刷新率是 75Hz，那么这个时间间隔就变成了 1000/75=13.3ms，换句话说就是
+   ，requestAnimationFrame 的步伐跟着系统的刷新步伐走。**它能保证回调函数在屏幕每一次的刷新间隔中值被执行一次** ，这样就
+   不不会引起丢帧现象，也不会出现导致动画卡顿的情况。
+
+API 调用：
+
+```javascript
+var progress = 0
+function render() {
+  //修改图像的位置
+  progress += 1
+  if (progress < 100) {
+    //在动画没有结束前，递归渲染
+    window.requestAnimationFrame(render)
+  }
+}
+
+//第一帧渲染
+window.requestAnimationFrame(render)
+```
+
+2. 使用 setTimeout实现的动画，当页面被影藏活最小化时，setTimeout仍然在后台执行动画，而requestAnimationFrame 则不同，当页面处理未激活的状态下，该页面的屏幕刷新任务会被暂停，因此跟着系统步伐走的requestAnimationFrame也会停止渲染，当页面被激活时，动画就从上次停留的地方继续执行，有效节省了CPU开销。
